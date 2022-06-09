a9572ccc9a31a6dfc4b09ec414074145ef56994e
Charlie R <calculovlog10@gmail.com>
2022-06-09 10:28:32 -0500

    Inicio del proyecto

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cafeteria - Inicio</title>
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+    <h1>CAFETERIA KONECTA</h1>
+    <div class="botones1">
+        
+
+    </div>
+    <div class="contenedor">
+        <table>
+
+
+        </table>
+    </div>
+
+</body>
+</html>
